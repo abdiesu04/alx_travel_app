4e1689bf23869e50481c87af303fd7d9d89c8fe0
abdiesu04 <abdiesayasbayisa@gmail.com>
2024-12-08 10:37:03 +0300

    Enable CORS for all origins in settings.py

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'drf_yasg',
 ]
 
+
+CORS_ALLOW_ALL_ORIGINS = True 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
